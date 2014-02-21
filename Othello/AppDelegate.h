a900e045b1434c4aed96a77f70f5edc1770f368c
USER <user@USER-no-MacBook-Air-2.local>
2014-02-21 11:12:52 +0900

    Initial Commit

---
+++ Othello/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Othello
+//
+//  Created by USER on 2014/02/21.
+//  Copyright (c) 2014年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Othello/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Othello
+//
+//  Created by USER on 2014/02/21.
+//  Copyright (c) 2014年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
